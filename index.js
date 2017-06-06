5788b4214554eea6323dbc74aa85672ad9e7031e
Phil Nash <philnash@gmail.com>
2017-06-06 11:53:49 +0100

    Updates to the latest twilio Node module

--- index.js
+++ index.js
@@ -5,30 +5,38 @@ const PORT = process.env.PORT || 3000;
 
 let app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
 app.all('/greeting', (req, res) => {
-    let twiml = new twilio.TwimlResponse();
-    twiml.say("There's someone at the door", { voice: "alice" });
-    res.type("text/xml");
+    let twiml = new twilio.twiml.VoiceResponse();
+    twiml.say("There's someone at the door", { voice: 'alice' });
+    res.type('text/xml');
     res.send(twiml.toString());
 });
 
 app.get('/call', (req, res) => {
-    let client = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    let client = new twilio(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_AUTH_TOKEN
+    );
     let greetingUrl = `${req.protocol}://${req.get('host')}/greeting`;
-    console.log(greetingUrl)
+    console.log(greetingUrl);
 
-    client.calls.create({
-        url: greetingUrl,
-        to: process.env.MY_NUMBER,
-        from: process.env.TWILIO_NUMBER
-    },(err, call) => {
-        console.log(err);
-        res.send(call.sid);
-    });
+    client.calls.create(
+        {
+            url: greetingUrl,
+            to: process.env.MY_NUMBER,
+            from: process.env.TWILIO_NUMBER
+        },
+        (err, call) => {
+            if (err) {
+                console.log(err);
+            }
+            res.send(call.sid);
+        }
+    );
 });
 
 app.listen(PORT, () => {
